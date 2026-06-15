9803a8f300cb49e62e6708231b7c5749dd44a42f
tmihung <tmihung@gmail.com>
2026-06-15 19:36:15 +0700

    Connect survey to Google Apps Script storage

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Cấu hình khi triển khai. Không đặt mật khẩu, API key hoặc secret trong file này.
 window.SURVEY_CONFIG = {
-  apiEndpoint: '',
-  resultEmail: ''
+  apiEndpoint: 'https://script.google.com/macros/s/AKfycbwUOCCVD17oitR-ntSliIOd-PaKZf57yKhqCaQNJ41HrFvYdc52EskZuHuRckR5x4OL/exec',
+  resultEmail: 'tmihung@gmail.com'
 };
